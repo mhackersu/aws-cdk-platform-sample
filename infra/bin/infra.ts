587fa9fc0a94884fadd595ace2d10d1a47f4a81d
Mike Hacker <mhackersu@gmail.com>
2022-09-19 14:36:15 -0500

    adding ecs cluster and vpc

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { InfraStack } from '../lib/infra-stack';
+import { EcsStack } from '../lib/infra-stack';
 
 const app = new cdk.App();
-new InfraStack(app, 'InfraStack', {
+new EcsStack(app, 'EcsStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
