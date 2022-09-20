fdc4b3e6cccb7c6b4441b423f1821adccda4d1d4
Mike Hacker <mhackersu@gmail.com>
2022-09-19 19:00:59 -0500

    fargate with local image

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { EcsStack } from '../lib/infra-stack';
+import { EcsStack } from '../lib/ecs-stack';
 
 const app = new cdk.App();
 new EcsStack(app, 'EcsStack', {
---
+++ infra/lib/ecs-stack.ts
@@ -0,0 +1,44 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as ecs from "aws-cdk-lib/aws-ecs";
+import * as ecs_patterns from "aws-cdk-lib/aws-ecs-patterns";
+
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class EcsStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'InfraQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+    const bucket = new s3.Bucket(this, `inf-s3-bucket`, {
+      versioned: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true
+    });
+
+    const vpc = new ec2.Vpc(this, "EcsVpc", {
+      maxAzs: 3 // Default is all AZs in region
+    });
+
+    const cluster = new ecs.Cluster(this, "EcsCluster", {
+      vpc: vpc
+    });
+
+    // Create a load-balanced Fargate service and make it public
+    new ecs_patterns.ApplicationLoadBalancedFargateService(this, "FgSvc", {
+      cluster: cluster, // Required
+      cpu: 512, // Default is 256
+      desiredCount: 6, // Default is 1
+      taskImageOptions: { image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample") },
+      memoryLimitMiB: 2048, // Default is 512
+      publicLoadBalancer: true // Default is true
+    });
+  }
+}
